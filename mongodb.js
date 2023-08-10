09e3cd3ff0233ef27705f4cc3274f18d7ee033a3
Christian E <christianelena92@gmail.com>
2023-08-10 13:07:01 -0500

    Udpate document using updateOne(), update the fields with : {}, : 1, Using promise

--- mongodb.js
+++ mongodb.js
@@ -6,47 +6,24 @@ async function connectToDB() {
         const client = await MongoClient.connect(connectionURL, { useNewUrlParser: true });
         const db = client.db(databaseName);   
        // C. Create 
-        const tasks = await db.collection('tasks').insertMany([{ description: 'code', completed: true }, { description: 'exercise', completed: false }, {description: 'sleep', completed: false }])
-        console.log(tasks);         
-/**
- *     // READ  - Querying Documents 
-
-db.collection('users').findOne({ name: 'jasibe' }, (error, user) => {
-    if (error) {
-        return console.log('unable error')
-    }
-    console.log(user);
-})
-
-        const insertedDocument = await db.collection('users').findOne({ _id: new ObjectId('64d4701366b1114c9ba6f54d') });
-        console.log(insertedDocument);
-
-// we get a cursor with find (its a pointer to (data) mongodb database or other forms of aggrigations)
+        // const tasks = await db.collection('tasks').insertMany([{ description: 'code', completed: true }, { description: 'exercise', completed: false }, {description: 'sleep', completed: false }])\
         
-let d = await db.collection('users').find({ age: 30 }).toArray((error, users) => {
-    if (error) {
-        console.error('Error while finding documents:', error);
-    } else {
-        console.log(users);
-    }
-});
+        // R.  READING DATA | QUERYING DOCUMENTS
+        // let lastTask = await db.collection('tasks').findOne({ _id: new ObjectId('64d5058b6dfea63fd728f2bd') });
         
-let x = db.collection('users').find({ age: 30 }).count((error, count) => {
-    if (error) {
-        console.error('Error while finding documents:', error);
-    } else {
-        console.log('count:', count);
-    }
-});   
-        console.log(x); 
- **/
- 
-    // R. QUERYING DOCUMENTS / READING DATA
-        let lastTask = await db.collection('tasks').findOne({ _id: new ObjectId('64d5058b6dfea63fd728f2bd') });
-        let completedTask = await db.collection('tasks').find({ completed: false }).toArray((error, collections) => console.log(collections)); 
-        let count = await db.collection('tasks').find({ completed: true }).count((err, countCollections) => console.log(countCollections));
+        // U.  UPDATE Documents | $set: {} , $inc: 1  
+        db.collection('users').updateOne({
+            _id: new ObjectId('64d498d6487bf74d6a7cc9fc')
+        }, {
+            $inc: {
+                age: 100
+            }
+        }).then((result) => {
+            console.log(result);
+        }).catch((error) => {
+            console.log(error);
+        }); 
 
-        console.log(count); 
     } catch (error) {
         console.error('Unable to connect to database', error);
     }
