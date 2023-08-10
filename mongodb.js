c72a18f63ae67f08989b92fcb2143a2ef9278263
Christian E <christianelena92@gmail.com>
2023-08-10 00:49:47 -0500

    Create, 3 task Collection with 2 fields one string and a boolean to determing weather or not it was accompleted,log the result

--- mongodb.js
+++ mongodb.js
@@ -8,28 +8,17 @@ const databaseName = 'task-manager'; // dbName
 async function connectToDB() {
     try {
         const client = await MongoClient.connect(connectionURL, { useNewUrlParser: true });
-        const db = client.db(databaseName); 
-        // insert a collection user  with .insertOne method & insert fields
-         /**
-         const result = await db.collection('users').insertOne({ name: 'Christian',  age: 30 }); 
-         - console.log(results.insertedId) for insertOne
-        const insertedDocument = await db.collection('users').findOne({ _id: result.insertedId });
-        console.log(insertedDocument);
-        */
+        const db = client.db(databaseName);   
         
-        // call the insertMany method on our user collection to insert many users fields into the collection at once, log the _id values of the inserted documents
-         const result = await db.collection('users').insertMany([
-            {
-            name: 'jasibe', 
-            age: 28
-            },
-            {
-                name: 'christian',
-                age: 5
-            }
-        ]); 
-          console.log(result.insertedIds);
- 
+
+        // Creating task
+        const result = await db.collection('task').insertMany([
+            { description: 'code', completed: false },
+            { description: 'work-out', completed: true },
+            { description: 'sleep', completed: false }
+        ]);
+        console.log(result.insertedIds);
+        
     } catch (error) {
         console.error('Unable to connect to database', error);
     }
