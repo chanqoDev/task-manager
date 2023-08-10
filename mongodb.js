32503314605062476ec592bce2d6a13766e50faa
Christian E <christianelena92@gmail.com>
2023-08-10 14:02:59 -0500

    Delete documents with fileds that match the critria using promises and applying deleteOne, deleteMany to our collection

--- mongodb.js
+++ mongodb.js
@@ -4,43 +4,64 @@ const { MongoClient, ObjectId } = require('mongodb');
 async function connectToDB() {
     try {
         const client = await MongoClient.connect(connectionURL, { useNewUrlParser: true });
-        const db = client.db(databaseName);   
-       // C. Create 
-        // const tasks = await db.collection('tasks').insertMany([{ description: 'code', completed: true }, { description: 'exercise', completed: false }, {description: 'sleep', completed: false }])\
+        const db = client.db(databaseName);
+        // C. Create  .insertOne()  .insertMany()
+        // const tasks = await db.collection('tasks').insertMany([
+        //     { description: 'code', completed: true },
+        //     { description: 'exercise', completed: false },
+        //     { description: 'sleep', completed: false }
+        // ])
         
-        // R.  READING DATA | QUERYING DOCUMENTS
+        // R.  READING DATA | QUERYING DOCUMENTS .findOne() || .findMany()
         // let lastTask = await db.collection('tasks').findOne({ _id: new ObjectId('64d5058b6dfea63fd728f2bd') });
         
-        // U.  UPDATE Documents | $set: {} , $inc: 1
+        // U.  UPDATE DOCUMENTS & Fields : update Documents.udpateOne().udpateMany() || date Fields => $set: { } , $inc: 1
+        /**
+        db.collection('users').updateOne({
+            _id: new ObjectId('64d498d6487bf74d6a7cc9fc')
+        }, {
+            $inc: {
+                age: 100
+            }
+        }).then((result) => {
+            console.log(result);
+        }).catch((error) => {
+            console.log(error);
+        });
 
-        // db.collection('users').updateOne({
-        //     _id: new ObjectId('64d498d6487bf74d6a7cc9fc')
-        // }, {
-        //     $inc: {
-        //         age: 100
-        //     }
-        // }).then((result) => {
-        //     console.log(result);
-        // }).catch((error) => {
-        //     console.log(error);
-        // });
-
-        
-
-
-
-
-        // U.  UPDATE DOCUMENTS & Fields 
-            // update Documents.udpateOne().udpateMany() || dpate Fields => $set: { } , $inc: 1
-
-        db.collection('tasks').updateMany(
-            { completed: false },
-            { $set: { completed: true } }
-        ).then((result) => {
-            console.log(result); 
+        db.collection('tasks').updateMany({ completed: false }, { $set: { completed: true } }).then((result) => {
+            console.log(result)
         }).catch((error) => {
             console.log('Updating document error failed', error)
         })
+        */
+
+
+
+        // D. DELETE DOCUMENTS & FIELDS : deleteMany({}) || .deleteOne({})
+        // db.collection('users').deleteMany({
+        //     age: 28
+        // }).then((result) => {
+        //     console.log(result)
+        // }).catch((error) =>
+        // { console.log('error:', error) });
+
+
+
+        
+
+
+        // delete one document from task using .deleteOne
+        db.collection('tasks').deleteOne({
+            _id: new ObjectId('64d50ac949d739de7da6d6d0')
+        }).then(result => {
+            console.log(result)
+        }).catch(err => console.error(err)); 
+
+
+
+
+
 
 
 
