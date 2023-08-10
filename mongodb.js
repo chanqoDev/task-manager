ce89dc92f21a418b2de59e84180d6db5a5a549b5
Christian E <christianelena92@gmail.com>
2023-08-10 00:33:20 -0500

    call the insertMany method on our user collection to insert many users fields into the collection at once, log the _id values of the inserted documents

--- mongodb.js
+++ mongodb.js
@@ -9,14 +9,27 @@ async function connectToDB() {
     try {
         const client = await MongoClient.connect(connectionURL, { useNewUrlParser: true });
         const db = client.db(databaseName); 
-        // insert a collection users
-        db.collection('users').insertOne({
-            // inserting fields
-            name: 'Christian', 
-            age: 30
-        }); 
-
-        // console.log('Connected correctly!');
+        // insert a collection user  with .insertOne method & insert fields
+         /**
+         const result = await db.collection('users').insertOne({ name: 'Christian',  age: 30 }); 
+         - console.log(results.insertedId) for insertOne
+        const insertedDocument = await db.collection('users').findOne({ _id: result.insertedId });
+        console.log(insertedDocument);
+        */
+        
+        // call the insertMany method on our user collection to insert many users fields into the collection at once, log the _id values of the inserted documents
+         const result = await db.collection('users').insertMany([
+            {
+            name: 'jasibe', 
+            age: 28
+            },
+            {
+                name: 'christian',
+                age: 5
+            }
+        ]); 
+          console.log(result.insertedIds);
+ 
     } catch (error) {
         console.error('Unable to connect to database', error);
     }
