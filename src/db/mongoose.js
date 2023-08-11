c5d90264c57a02d788ca2c3a4cad438f0a6da2bd
Christian E <christianelena92@gmail.com>
2023-08-11 00:48:15 -0500

    feat: Implement task creation endpoint and model separation
    This commit introduces the following enhancements to the application:
    1. Create a separate file for the Task model and load it into the model index.js for better organization and separation of concerns.
    2. Implement a task creation endpoint that handles both success and error cases. The endpoint allows tasks to be created by sending POST requests.
    3. Test the task creation endpoint with various data scenarios, including valid and invalid inputs.
    
    Changes introduced in this commit:
    - Move the Task model definition to a separate file for better code organization
    - Update the model index.js to include the new Task model file
    - Implement a new endpoint for task creation, handling success and error cases
    - Test the task creation endpoint using different data inputs

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,17 +4,3 @@ const { db: { connectionURL } } = require('../../config');
 mongoose.connect(`${connectionURL}/task-manager-api`, {
     useNewUrlParser: true
 }); 
-
-
- // 1. Task model with description and completed fields
-const Tasks = mongoose.model("Tasks", {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false,
-    }
-}); 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 const express = require('express'); 
 require('../src/db/mongoose');
 const User = require('./models/user'); 
+const Tasks = require("./models/tasks"); 
 
 const app = express(); 
 const port = process.env.PORT || 3000; 
 app.use(express.json());
 
-
-
-app.post('/users', (req, res) => {
-    const user = new User(req.body); 
-
-    user.save().then(() => {
-        res.send(user); 
+app.post('/tasks', (req, res) => {
+    const tasks = new Tasks(req.body); 
+    tasks.save().then(() => {
+        res.status(201).res().send(tasks); 
     }).catch((error) => {
-        res.status(400).send(error)
+        res.status(400).send(error); 
     })
-});
+}); 
+
+// app.post('/users', (req, res) => {
+//     const user = new User(req.body); 
+//     user.save().then(() => {
+//          res.status(201).send(user); 
+//     }).catch((error) => {
+//         res.status(400).send(error)
+//     })
+// });
 
  
 app.listen(port, () => {
