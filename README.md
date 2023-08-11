fa0087d2b69df6000318797f96ec01479b3480a3
Christian E <christianelena92@gmail.com>
2023-08-10 19:38:24 -0500

    update README.md, install validator lib, Add User model with validation and trimming
    Created the User model with the following changes:
    - Added 'name' field with required and trim options
    - Added 'age' field with default value and positive number validation
    - Added 'email' field with required, trim, lowercase, and email validation
    - Added 'password' field with required, minlength, trim, and custom validation
    - Created an instance of User with sample data
    
    This commit enhances the User model with validation rules and trims input data for consistency.

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ CRUD Create. Read. Update. Delete operations
 moongoes Docs : https://mongoosejs.com/docs/guide.html
 npm : https://www.npmjs.com/package/mongoose
 
-1. Install mongoose
+1. Install mongoose depencies
     
-> Install depencies : npm install mongoose
+>  npm install mongoose
+
+2. Install Validator depencies https://www.npmjs.com/package/validator
+
+> npm i validator
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,53 +1,75 @@
 const mongoose = require("mongoose"); 
 const { db: { connectionURL } } = require('../../config');
-// connect Mongoose to the Database 
+const validator = require('validator'); 
+
 mongoose.connect(`${connectionURL}/task-manager-api`, {
     useNewUrlParser: true
 }); 
 
- // 1. Define a model with description and completed fields
-const Tasks = mongoose.model("Tasks", {
-    description: {
-        type: String
-    }, 
-    completed: {
-        type: Boolean
-    }
-})
- // 2. Create a new Instance of the Model
-const task = new Tasks({
-    description: "Reading a Book",
-    completed: true
-})
- // 3. Save the Model to the db
-task.save().then(() => {
-     console.log(task)
- }).catch((err) => {console.log('error:', err)})
+ // 1. Task model with description and completed fields
+// const Tasks = mongoose.model("Tasks", {
+//     description: {
+//         type: String,
+//     }, 
+//     completed: {
+//         type: Boolean
+//     }
+// })
+//  // 2. Create a new Instance of the Model
+// const task = new Tasks({
+//     description: "Reading a Book",
+//     completed: true
+// })
+//  // 3. Save the Model to the db
+// task.save().then(() => {
+//      console.log(task)
+//  }).catch((err) => {console.log('error:', err)})
  
 
 
 
 
+// user model 
 
-// we define our model User
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// }); 
+const User = mongoose.model('User', {
+    name: { type: String, required: true, trim: true },
+     age: { type: Number, default: 0, 
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number'); 
+            }
+        }
+    },
+    email: { type: String, required: true, trim: true, lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');  
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if ( value.toLowerCase().includes('password')) {
+                 throw new Error('Password cannot contain Password')
+             }  
+        }
+   }, 
+}); 
 
-// const me = new User({
-//     name: 'Chris',
-//     age: 18
-// }); 
+const me = new User(
+    {
+        name: '   Joe Tortilla   ',
+        email: ' franchesco@gmail.com',
+        password: '  meowPasswerd  '
+    }); 
 
-// save it to the database or trow an errow
-// me.save().then(() => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log("Error!", error);
-// });
+me.save().then(() => {
+    console.log(me);
+}).catch((error) => {
+    console.log("Error!", error);
+});
 
